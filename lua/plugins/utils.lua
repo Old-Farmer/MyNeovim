4f9d112f84ee3152ff795c13b96bf2331c2c75d7
Charlie <1667482085@qq.com>
2024-03-22 15:42:41 +0800

    Changes

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -3,4 +3,27 @@ return {
     "dstein64/vim-startuptime",
     enabled = false,
   },
+  -- Bookmarks
+  {
+    "LintaoAmons/bookmarks.nvim",
+    dependencies = {
+      { "stevearc/dressing.nvim" }, -- optional: to have the same UI shown in the GIF
+    },
+    event = "BufEnter",
+    keys = {
+      {
+        "<leader>mm",
+        "<cmd>BookmarksMark<cr>",
+        desc = "Mark current line into active BookmarkList",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>mg",
+        "<cmd>BookmarksGoto<cr>",
+        desc = "Go to bookmark at current active BookmarkList",
+        mode = { "n", "v" },
+      },
+      { "<leader>mc", "<cmd>BookmarksCommands<cr>", desc = "Find and trigger a bookmark command", mode = { "n", "v" } },
+    },
+  },
 }
