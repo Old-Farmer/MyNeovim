4a8c2bc5cea9648ceda4a39f7a487e8436f3dd97
Charlie <1667482085@qq.com>
2024-04-19 01:25:53 +0800

    Fri Apr 19 01:25:53 AM CST 2024

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
+-- local map = vim.keymap.set
 -- local mapdel = vim.keymap.del
-
-map({ "i", "n" }, "<C-;>", "<End>", { noremap = true, desc = "Line end" })
-map("i", "<C-S-;>", "<End>;", { noremap = true, desc = "Append ; to line end" })
--- map("i", "<C-S-k>", "<C-k><C-k>i<BS>", { desc = "Close signature help" }) -- don't know why need bs
