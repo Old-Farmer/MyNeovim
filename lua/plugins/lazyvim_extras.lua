9153f9e54745f9ea009542754d7248857c9ba1b4
Charlie <1667482085@qq.com>
2024-04-19 10:03:26 +0800

    Fri Apr 19 10:03:26 AM CST 2024

--- lua/plugins/lazyvim_extras.lua
+++ lua/plugins/lazyvim_extras.lua
@@ -1,6 +1,7 @@
 return {
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.dap.nlua" },
+  { import = "lazyvim.plugins.extras.editor.trouble-v3" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.lang.cmake" },
   -- { import = "lazyvim.plugins.extras.lang.go" },
