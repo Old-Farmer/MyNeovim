36b2fa43d78b4c879606f88eedee3af62071a4bf
Charlie <1667482085@qq.com>
2024-06-03 00:49:43 +0800

    Mon Jun  3 12:49:43 AM CST 2024

--- lua/plugins/input_method.lua
+++ lua/plugins/input_method.lua
@@ -1,21 +1,14 @@
 return {
-  -- {
-  --   "h-hg/fcitx.nvim",
-  --   -- event = "VeryLazy",
-  -- },
-  -- {
-  --   "keaising/im-select.nvim",
-  --   config = function()
-  --     require("im_select").setup({})
-  --   end,
-  -- },
   {
     "Old-Farmer/im-autoswitch.nvim",
-    event = "BufEnter",
+    -- dir = "~/projects/im-autoswitch.nvim",
+    event = "VeryLazy",
     opts = {
-      default_im = "1",
-      get_im_cmd = "fcitx5-remote",
-      switch_im_cmd = "fcitx5-remote -t",
+      cmd = {
+        default_im = "1",
+        get_im_cmd = "fcitx5-remote",
+        switch_im_cmd = "fcitx5-remote -t",
+      },
     },
   },
 }
